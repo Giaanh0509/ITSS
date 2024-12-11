81b8945eb6a27db3c2dfadbd68d033d2a164d344
Hoang Phong <hoangphong210703@gmail.com>
2024-12-11 15:17:34 +0700

    fix-eye_icon-RegisterPage

--- frontend/src/pages/Register/RegistrationForm.jsx
+++ frontend/src/pages/Register/RegistrationForm.jsx
@@ -1,129 +1,150 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Register.css';
 import UserService from "../../services/UserService";
 
+import eyePasswordShow from "../../assets/eye-password-show.png";
+import eyePasswordHide from "../../assets/eye-password-hide.png";
+
+
 const RegistrationForm = () => {
-  const navigate = useNavigate(); // Initialize navigate function
-  
-  const [user, setUser] = useState({
-    id: "",
-    username: "",
-    password: "",
-    email: "",
-    name: "",
-  });
+    const navigate = useNavigate();
 
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+    const [user, setUser] = useState({
+        id: "",
+        username: "",
+        password: "",
+        email: "",
+        name: "",
+    });
 
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setUser({ ...user, [e.target.name]: value });
-  };
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
-  const handleConfirmPasswordChange = (e) => {
-    setConfirmPassword(e.target.value);
-  };
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setUser({ ...user, [e.target.name]: value });
+    };
 
-  const saveUser = (e) => {
-    e.preventDefault();
+    const handleConfirmPasswordChange = (e) => {
+        setConfirmPassword(e.target.value);
+    };
 
-    if (user.password !== confirmPassword) {
-      setErrorMessage("パスワードが一致しません。もう一度確認してください。");
-      console.log(errorMessage);
-      return;
-    }
+    const togglePasswordVisibility = () => {
+        setShowPassword((prevState) => !prevState);
+    };
 
-    UserService.saveUser(user)
-      .then((response) => {
-        console.log(response);
-        setUser({
-          id: "",
-          username: "",
-          password: "",
-          email: "",
-          name: "",
-        });
+    const saveUser = (e) => {
+        e.preventDefault();
 
-        e.target.reset();
-        alert("登録に成功しました！ログイン画面に遷移します。");
-        navigate("/login"); // Navigate to login page after successful registration
-      })
-      .catch((err) => {
-        console.error(err);
-        setErrorMessage("登録に失敗しました。もう一度お試しください。");
-      });
-  };
+        if (user.password !== confirmPassword) {
+            setErrorMessage("パスワードが一致しません。もう一度確認してください。");
+            return;
+        }
 
-  return (
-    <form className="registration-form" onSubmit={saveUser}>
-      <div className="form-group">
-        <label>名前</label>
-        <input
-          type="text"
-          name="name"
-          value={user.name}
-          onChange={(e) => handleChange(e)}
-          placeholder="名前を入力してください"
-        />
-      </div>
-      <div className="form-group">
-        <label>ユーザー名</label>
-        <input
-          type="text"
-          name="username"
-          value={user.username}
-          onChange={(e) => handleChange(e)}
-          placeholder="ユーザー名を入力してください"
-        />
-      </div>
-      <div className="form-group">
-        <label>パスワード</label>
-        <div className="password-input">
-          <input
-            type="password"
-            name="password"
-            value={user.password}
-            onChange={(e) => handleChange(e)}
-            placeholder="パスワードを入力してください"
-          />
-          <img src="../../assets/images/eye_icon.svg" alt="toggle visibility" />
-        </div>
-      </div>
-      <div className="form-group">
-        <label>パスワード</label>
-        <div className="password-input">
-          <input
-            type="password"
-            name="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => handleConfirmPasswordChange(e)}
-            placeholder="もう一度パスワードを入力してください"
-          />
-          <img src="../../assets/images/eye_icon.svg" alt="toggle visibility" />
-        </div>
-      </div>
-      <div className="form-group">
-        <label>メールアドレス</label>
-        <input
-          type="email"
-          name="email"
-          value={user.email}
-          onChange={(e) => handleChange(e)}
-          placeholder="メールアドレスを入力してください"
-        />
-      </div>
-      <button type="submit" className="submit-button">登録</button>
-      {errorMessage && (
-        <div className="error-message">{errorMessage}</div>
-      )}
-      <div className="login-link">
-        <a href="/login">ログイン画面に遷移する</a>
-      </div>
-    </form>
-  );
+        UserService.saveUser(user)
+            .then((response) => {
+                setUser({
+                    id: "",
+                    username: "",
+                    password: "",
+                    email: "",
+                    name: "",
+                });
+
+                alert("登録に成功しました！ログイン画面に遷移します。");
+                navigate("/login", { replace: true });
+            })
+            .catch((err) => {
+                setErrorMessage("登録に失敗しました。もう一度お試しください。");
+            });
+    };
+
+    return (
+        <form className="registration-form" onSubmit={saveUser}>
+            <div className="form-group">
+                <label htmlFor="name">名前</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={user.name}
+                    onChange={handleChange}
+                    placeholder="名前を入力してください"
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="username">ユーザー名</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={user.username}
+                    onChange={handleChange}
+                    placeholder="ユーザー名を入力してください"
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">パスワード</label>
+                <div className="password-input">
+                    <input
+                        type={showPassword ? "text" : "password"}
+                        id="password"
+                        name="password"
+                        value={user.password}
+                        onChange={handleChange}
+                        placeholder="パスワードを入力してください"
+                    />
+                    <img
+                        src={showPassword ? eyePasswordShow : eyePasswordHide}
+                        alt="toggle visibility"
+                        onClick={togglePasswordVisibility}
+                        style={{ cursor: "pointer" }}
+                    />
+
+                </div>
+            </div>
+            <div className="form-group">
+                <label htmlFor="confirmPassword">パスワード（確認用）</label>
+                <div className="password-input">
+                    <input
+                        type={showPassword ? "text" : "password"}
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        value={confirmPassword}
+                        onChange={handleConfirmPasswordChange}
+                        placeholder="もう一度パスワードを入力してください"
+                    />
+                    <img
+                        src={showPassword ? eyePasswordShow : eyePasswordHide}
+                        alt="toggle visibility"
+                        onClick={togglePasswordVisibility}
+                        style={{ cursor: "pointer" }}
+                    />
+                </div>
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">メールアドレス</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={user.email}
+                    onChange={handleChange}
+                    placeholder="メールアドレスを入力してください"
+                />
+            </div>
+            <button type="submit" className="submit-button">登録</button>
+            {errorMessage && (
+                <div className="error-message">{errorMessage}</div>
+            )}
+            <div className="login-link">
+                <a href="/login">ログイン画面に遷移する</a>
+            </div>
+        </form>
+    );
 };
 
 export default RegistrationForm;
