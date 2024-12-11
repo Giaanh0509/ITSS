8c3beae6664af9e7f43ee7fcdd32e28abe00d766
hoangQuan25 <quandz250203@gmail.com>
2024-12-11 23:27:06 +0700

    add paging backend

--- backend/src/main/java/com/example/project/dao/FavoritesRepository.java
+++ backend/src/main/java/com/example/project/dao/FavoritesRepository.java
@@ -1,6 +1,8 @@
 package com.example.project.dao;
 
 import com.example.project.entity.Favorite;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +15,7 @@ public interface FavoritesRepository extends JpaRepository<Favorite, Integer> {
     List<Favorite> findTop4ByUserIdOrderByAddDateDesc(int userId);
 
 
-    List<Favorite> findByUserId(int userId);
+    Page<Favorite> findByUserId(int userId, Pageable pageable);
 
     Optional<Favorite> findByUserIdAndFoodId(int userId, int foodId);
 }
--- backend/src/main/java/com/example/project/service/FavoritesService.java
+++ backend/src/main/java/com/example/project/service/FavoritesService.java
@@ -2,6 +2,7 @@ package com.example.project.service;
 
 import com.example.project.dto.FavoriteDto;
 import com.example.project.entity.Favorite;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,8 +11,7 @@ public interface FavoritesService {
 
     List<FavoriteDto> findMostRecentFavoritesByUsername(String username);
 
-    //
-    List<FavoriteDto> findFavoritesByUsername(String username);
+    Page<FavoriteDto> findFavoritesByUsername(String username, int page, int size);
 
     void addFavorite(Favorite favorite);
 
