8a0a202c265107a13c2c6c8a157521ec7fcd878d
Hoang Phong <hoangphong210703@gmail.com>
2024-12-29 20:35:44 +0700

    Fix query

--- backend/src/main/java/com/example/project/dao/FoodsRepository.java
+++ backend/src/main/java/com/example/project/dao/FoodsRepository.java
@@ -11,7 +11,21 @@ import java.util.List;
 @Repository
 public interface FoodsRepository extends JpaRepository<Food, Integer> {
 
-    @Query("SELECT DISTINCT f FROM Food f JOIN f.tags t WHERE t.tagName IN :tags")
+//    @Query("SELECT DISTINCT f FROM Food f JOIN f.tags t WHERE t.tagName IN :tags")
+//    List<Food> findFoodsByTags(@Param("tags") List<String> tags);
+
+    @Query("""
+            SELECT DISTINCT f
+            FROM Food f
+            JOIN f.tags t
+            WHERE t.tagName IN :tags
+              AND NOT EXISTS (
+                  SELECT 1
+                  FROM f.tags t2
+                  WHERE t2.tagName IN ('ingre_sea', 'ingre_egg', 'ingre_dairy', 'ingre_meat')
+              )
+            """)
     List<Food> findFoodsByTags(@Param("tags") List<String> tags);
+    
 }
 
